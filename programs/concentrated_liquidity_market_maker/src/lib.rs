b402a6de2db375edb8c5842622f44946716d1b4c
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2026-01-15 23:11:32 +0530

    added entry level functions

--- programs/concentrated_liquidity_market_maker/src/lib.rs
+++ programs/concentrated_liquidity_market_maker/src/lib.rs
@@ -6,11 +6,37 @@ declare_id!("A8ik9zk5rnhK9EDX75jbkMEqh4sTb7TkYcwVqUsHCcgR");
 pub mod concentrated_liquidity_market_maker {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+   #[program]
+pub mod concentrated_liquidity_market_maker {
+    use super::*;
+    
+    pub fn initialize_pool(ctx: Context<InitializePool>) -> Result<()> {
+        Ok(())
+    }
+    
+    pub fn open_position(ctx: Context<OpenPosition>) -> Result<()> {
+        Ok(())
+    }
+    
+    pub fn add_liquidity(ctx: Context<AddLiquidity>) -> Result<()> {
+        Ok(())
+    }
+    
+    pub fn remove_liquidity(ctx: Context<RemoveLiquidity>) -> Result<()> {
+        Ok(())
+    }
+    
+    pub fn swap(ctx: Context<Swap>) -> Result<()> {
+        Ok(())
+    }
+    
+    pub fn collect_fees(ctx: Context<CollectFees>) -> Result<()> {
+        Ok(())
+    }
+    
+    pub fn close_position(ctx: Context<ClosePosition>) -> Result<()> {
         Ok(())
     }
 }
+}
 
-#[derive(Accounts)]
-pub struct Initialize {}
